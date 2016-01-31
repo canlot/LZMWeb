a9f02494ddbade83d12feeb0ae2f4dcfa330607b
canlot <jakobsasmuzki@gmx.de>
2016-01-31 21:56:14 +0100

    User hinzugefügt, Sql queries zum abfragen der Links hinzugefügt

--- GetLinks.php
+++ GetLinks.php
@@ -26,7 +26,31 @@ class GetLinks
     }
     public function getLinks($theme, $user = FALSE)
     {
+        $query = "select Links.link from Links " +
+                "inner join Relation " +
+                "on Links.id = Relation.link " +
+                "inner join Theme " +
+                "on Theme.id = Relation.theme " +
+                "where Theme.theme = '" + $theme + "'";
+        if($user != FALSE)
+            $query += " and Links.user = " + $user->id + "";
+        
+        $result = $this->mysqli->query($query);
+        
+        $return = array();
+        if($result->num_rows > 0)
+        {
+            while ($row = $result->fetch_assoc())
+            {
+                array_push($return, $row["link"]);
+            }
+        }
+        return $return;
         
     }
-    //put your code here
+    
+    public function __destruct()
+    {
+        $this->mysqli->close();
+    }
 }
