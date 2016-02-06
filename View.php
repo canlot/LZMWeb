1958de795beb01e44ebcd99022eec9eaeb12f022
canlot <jakobsasmuzki@gmx.de>
2016-02-07 00:01:47 +0100

    Templates erweitert, nutzlose Information zu Template hinzugefügt. Erste lauffähige Version.

--- View.php
+++ View.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'config/TemplateInformation.php';
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,7 +11,10 @@
  *
  * @author Jake
  */
-class View 
+class View
 {
-    //put your code here
+    public function show()
+    {
+        require 'templates/' . \TInfo\Template . '/index.php';
+    }
 }
