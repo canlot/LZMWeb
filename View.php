f5a121400a8aeb672d359c26789684ab04c09f52
canlot <jakobsasmuzki@gmx.de>
2016-02-22 08:51:49 +0100

    Einige Änderungen

--- View.php
+++ View.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'config/TemplateInformation.php';
 require_once 'libs/defines/DatabaseQueries.php';
+require_once 'libs/Modules/GetLinks.php';
+require_once 'Database/Database.php';
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,9 +18,13 @@ class View
 {
     private $data = array();
     private $controller = null;
+    private $modules = array();
+    private $database = null;
     public function __construct(Controller $controller)
     {
+        //$this->database = new DB\Database()
         $this->controller = $controller;
+        //$this->modules[] = new GetLinks($database)
     }
     public function processRequestAndData()
     {
