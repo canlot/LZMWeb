7b3ed829222b64621fb67be2bd11e79ad3f9ae4f
canlot <jakobsasmuzki@gmx.de>
2016-07-12 10:11:05 +0200

    Neue Klasse hinzugefügt

---
+++ libs/Modules/SetLinks.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'Module.php';
+class SetLinks extends Module
+{
+    public function __construct(\DB\Database $database) 
+    {
+        parent::__construct($database);
+    }
+    public function returnData($querys)
+    {
+        
+    }
+}
